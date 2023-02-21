4b1d15648a9f20eb48a1592dd37fa3dd7752bdc8
tirth.temp <tirth.m@devhostserver.com>
2023-02-21 13:46:32 +0530

    "todo app"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+    let tempid = 1;
+
+    const [todo, settodo] = useState({text:""});
+    const [todos,settodos] =useState([]);
+
+     const oninputchange = (e) =>{
+      settodo({text:e.target.value});
+     }
+
+     const onsubmithandel = (e)=>{
+          e.preventDefault();
+          settodos(...todos,[{id: tempid , text: todo.text}]);
+          tempid = tempid+1;
+          console.log(todos,20);
+     } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form action="" onSubmit={onsubmithandel}>
+      <input type="text"  name='todoinput' onChange={oninputchange} />
+
+      <button type='submit'>add todo</button>
+      </form> 
+      
+
+      
+
+      </>
   );
 }
 
