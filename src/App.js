5aea333799537037904f299abcc62f2af518c8cc
tirth.temp <tirth.m@devhostserver.com>
2023-02-21 18:11:06 +0530

    "done changes"

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-
-    let tempid = 1;
-
-    const [todo, settodo] = useState({text:""});
+    const [todo, settodo] = useState("");
     const [todos,settodos] =useState([]);
-
+    const [isedit,setisedit] =useState(false);
      const oninputchange = (e) =>{
-      settodo({text:e.target.value});
+      settodo(e.target.value);
+      // console.log(e.target.value);
      }
-
      const onsubmithandel = (e)=>{
-          e.preventDefault();
-          settodos(...todos,[{id: tempid , text: todo.text}]);
-          tempid = tempid+1;
-          console.log(todos,20);
+          e.preventDefault();      
+          const newtodo ={
+              id: new Date().getTime(),
+              text:todo
+          }
+          settodos(todos.concat(newtodo));
+          settodo("");
+          // console.log(todos,23);
      } 
+    const ondelete =(id) =>{
+        // console.log(id,"delete");
+        todos.map((todo)=>{
+             settodos([...todos].filter((todo) => todo.id !== id))
+             return todos
+        })
+     }
+     const onupdate =(item) =>{
+      // console.log(item,"update");
+      settodo(item.text);
+      setisedit(true);  
+   }
+   const onupdatesave=(item)=>{
+      item.text = todo;
+      settodo("");
+      setisedit(false);
+   }
+
   return (
     <>
       <form action="" onSubmit={onsubmithandel}>
-      <input type="text"  name='todoinput' onChange={oninputchange} />
-
+      <input type="text" value={todo} name='todoinput' onChange={oninputchange} />
       <button type='submit'>add todo</button>
+      
       </form> 
-      
-
-      
-
+      {
+        todos.map((item)=>{
+          // console.log(item);
+            return (<div key ={item.id} >{item.text}
+            {!isedit?<button onClick={() => onupdate(item)}>update</button>: <button onClick={() => onupdatesave(item)}>Save</button>}
+            <button onClick={() => ondelete(item.id)}>delete</button></div>
+            )
+        })
+      }
       </>
   );
 }
